b120a9f892a44a2aa9fc5b46dfbb9b121207ffa2
Mason Guaman <masonstalin@gmail.com>
2023-09-19 14:00:33 -0400

    update

--- InterfaceBasicDoNow/ViewController.swift
+++ InterfaceBasicDoNow/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var button: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greet(_ sender: Any) {
+        
+        button.text = "Hello " + (textField.text ?? " ")
+    }
+    
+    
 }
 
